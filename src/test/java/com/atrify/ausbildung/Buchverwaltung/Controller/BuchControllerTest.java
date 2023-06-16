b8c8fc6b8f4f94bb442699fc15aecce70699fd25
Abdullah Gwadabe <agwadabe@atrify.com>
2023-06-16 11:35:23 +0200

    Initial Commit

---
+++ src/test/java/com/atrify/ausbildung/Buchverwaltung/Controller/BuchControllerTest.java
@@ -0,0 +1,28 @@
+package com.atrify.ausbildung.Buchverwaltung.Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BuchControllerTest {
+
+  @Test
+  void addBook() {
+  }
+
+  @Test
+  void updateBook() {
+  }
+
+  @Test
+  void getBookByISBN() {
+  }
+
+  @Test
+  void deleteBook() {
+  }
+
+  @Test
+  void getAllBooksInLibrary() {
+  }
+}
